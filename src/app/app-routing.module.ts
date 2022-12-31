8983b2289394bfadc86091965269396b4afd2266
katpure <priyankakatpure@gmail.com>
2022-12-31 18:24:49 +0530

    todays commot

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { MedicineHomeComponent } from './medicine-home/medicine-home.component';
+import { TopDealsComponent } from './top-deals/top-deals.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'/home',pathMatch:'full'},
+  {path:'home',component:HomeComponent},
+  {path:'medicine-home',component:MedicineHomeComponent},
+  // {path:'top-deals',component:TopDealsComponent},
+  {path:'**',component:PagenotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { MedicineHomeComponent } from './medicine-home/medicine-home.component';
+import {BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { TopDealsComponent } from './top-deals/top-deals.component';
+import { FormsModule } from '@angular/forms';
+import { CoreModule } from './core/core.module';
+import { HttpComponent } from './core/http/http.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    PagenotfoundComponent,
+    MedicineHomeComponent,
+    TopDealsComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CarouselModule,
+    FormsModule,
+    CoreModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
